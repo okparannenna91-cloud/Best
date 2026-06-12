10c2ece1e908e67f143e76dbbc984441c9af8eb7
okparannenna91-cloud <okparannenna91@gmail.com>
2026-06-12 10:57:05 +0100

    Add debug log for RESEND_API_KEY, ignore .vercel

--- config/email.js
+++ config/email.js
@@ -1,5 +1,7 @@
 const { Resend } = require('resend');
 
+console.log("RESEND_API_KEY:", !!process.env.RESEND_API_KEY);
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 const FROM = 'SOLLENE <noreply@sollene.site>';
 
